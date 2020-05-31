faf68befb58e5e59fdfd51ae53a5ad5efe66f8e6
gordon636798 <46336926+gordon636798@users.noreply.github.com>
2020-06-01 01:20:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ import json
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('66QuFSr22occhn5pWWZgiNHPlNiAkOmbPltgdKEwodeRi3ipqJhTnj4l4TILfrtgXcz1mrxOJjfnPBPeLgqZvrMm0UFJzo9wBahXTUFRhwTseht5pJg/3KVGkqL9uGSrKGdR669woiNKYg7Az01aawdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('f8b590c9c7ea8cbb59427a9aa4993a25')
+line_bot_api = LineBotApi('CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('CHANNEL_SECRET')
 
 users = {}
 
